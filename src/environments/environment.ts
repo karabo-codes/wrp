f7c2e5da39ed59ec2c04f97f0434106cca8e0518
desupink <geefuller5@gmail.com>
2023-12-03 17:01:39 +0200

    Firebase Initial Changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCp0PXfqKQiS3D7aS01Xpded8FFG59qsBI",
+    authDomain: "whizants-recruitment-platform.firebaseapp.com",
+    projectId: "whizants-recruitment-platform",
+    storageBucket: "whizants-recruitment-platform.appspot.com",
+    messagingSenderId: "248682740879",
+    appId: "1:248682740879:web:de5fdf86cd00ff663cc4fa",
+    measurementId: "G-JL629NHL37"
+  }
+};
